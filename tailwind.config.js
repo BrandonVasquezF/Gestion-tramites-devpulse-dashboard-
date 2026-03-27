58245cc3c3a4a863df6027e1dc119f933425563c
BrandonVasquezF <brandonvasquezf@gmail.com>
2026-03-27 18:43:03 -0500

    estructura inicial del dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+  // 1. CONTENT: El "Radar"
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // 2. THEME: Tu "Estilo Único"
+  theme: {
+    extend: {},
+  },
+  // 3. PLUGINS: Las "Expansiones"
+  plugins: [],
+}
